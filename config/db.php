45d65b5672f42cebd35ae1b943b5fe0fb9106241
bagus rasuli <basgu7rasuli@gmail.com>
2021-03-13 00:41:21 +0700

    config db

--- config/db.php
+++ config/db.php
@@ -1,14 +1,22 @@
 <?php
 
+// return [
+//     'class' => 'yii\db\Connection',
+//     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+//     'username' => 'root',
+//     'password' => '',
+//     'charset' => 'utf8',
+
+//     // Schema cache options (for production environment)
+//     //'enableSchemaCache' => true,
+//     //'schemaCacheDuration' => 60,
+//     //'schemaCache' => 'cache',
+// ];
+
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'pgsql:host=192.168.254.70;port=5432;dbname=simrs',
+    'username' => 'postgres',
+    'password' => '1satu2dua',
     'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
 ];
