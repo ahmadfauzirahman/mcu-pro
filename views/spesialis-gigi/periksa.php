690932d27ab91d60069f05b53cf01c9008501e63
bagus rasuli <basgu7rasuli@gmail.com>
2021-03-14 11:38:19 +0700

    form-control-sm nama - gigi

--- views/spesialis-gigi/periksa.php
+++ views/spesialis-gigi/periksa.php
@@ -117,7 +117,7 @@ $dataG = [
             <div class="col-sm-6">
               <div class="form-group">
                 <label for="">Nama</label>
-                <input readonly type="text" class="form-control" value="<?= $pasien->nama ?? null ?>" id="nama_pasien">
+                <input readonly type="text" class="form-control form-control-sm" value="<?= $pasien->nama ?? null ?>" id="nama_pasien">
               </div>
             </div>
             <div class="col-sm-3">
