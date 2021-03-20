2598312c0c868ec4ccc2922c4f2d074b28ddfb24
Dicky Ermawan S <wanasaja@gmail.com>
2021-03-20 14:45:13 +0700

    ref: bagi tab unit pemeriksaan fisik

--- views/unit-pemeriksaan/index.php
+++ views/unit-pemeriksaan/index.php
@@ -22,37 +22,42 @@ $this->params['breadcrumbs'][] = $this->title;
                     <h4 class="card-title">Unit Pemeriksaan Dokter Umum</h4>
                     <p class="card-title-desc">Unit Pemeriksaan DOkter Umum Untuk MCU</p>
 
+Data Pelayanan 
+<br>
+<br>
+<br>
+
                     <!-- Nav tabs -->
                     <ul class="nav nav-pills nav-justified" role="tablist">
                         <li class="nav-item waves-effect waves-light">
-                            <a class="nav-link active" data-toggle="tab" href="#home-1" role="tab">
+                            <a class="nav-link active" data-toggle="tab" href="#anamnesis" role="tab">
                                 <span class="d-block d-sm-none"><i class="fas fa-home"></i></span>
-                                <span class="d-none d-sm-block">Data Pelayanan</span>
-                            </a>
-                        </li>
-                        <li class="nav-item waves-effect waves-light">
-                            <a class="nav-link" data-toggle="tab" href="#profile-1" role="tab">
-                                <span class="d-block d-sm-none"><i class="far fa-user"></i></span>
                                 <span class="d-none d-sm-block">Anamnesis</span>
                             </a>
                         </li>
                         <li class="nav-item waves-effect waves-light">
-                            <a class="nav-link" data-toggle="tab" href="#messages-1" role="tab">
+                            <a class="nav-link" data-toggle="tab" href="#anamnesis-okupasi" role="tab">
+                                <span class="d-block d-sm-none"><i class="far fa-user"></i></span>
+                                <span class="d-none d-sm-block">Anamnesis Okupasi</span>
+                            </a>
+                        </li>
+                        <li class="nav-item waves-effect waves-light">
+                            <a class="nav-link" data-toggle="tab" href="#pemeriksaan-fisik" role="tab">
                                 <span class="d-block d-sm-none"><i class="far fa-envelope"></i></span>
                                 <span class="d-none d-sm-block">Pemeriksaan Fisik</span>
                             </a>
                         </li>
                         <li class="nav-item waves-effect waves-light">
-                            <a class="nav-link" data-toggle="tab" href="#settings-1" role="tab">
+                            <a class="nav-link" data-toggle="tab" href="#resume" role="tab">
                                 <span class="d-block d-sm-none"><i class="fas fa-cog"></i></span>
-                                <span class="d-none d-sm-block">Settings</span>
+                                <span class="d-none d-sm-block">Resume</span>
                             </a>
                         </li>
                     </ul>
 
                     <!-- Tab panes -->
                     <div class="tab-content p-3 text-muted">
-                        <div class="tab-pane active" id="home-1" role="tabpanel">
+                        <div class="tab-pane active" id="anamnesis" role="tabpanel">
                             <div class="card card-body">
                                 <h4 class="card-title">Anamnesis</h4>
                                 <p class="card-title-desc">Anamnesis yang baik harus mengacu pada pertanyaan yang sistematis, yaitu dengan berpedoman pada empat pokok pikiran (The Fundamental Four)dan tujuh butir mutiara anamnesis (The Sacred Seven)</p>
@@ -104,17 +109,17 @@ $this->params['breadcrumbs'][] = $this->title;
                                 </div>
                             </div>
                         </div>
-                        <div class="tab-pane" id="profile-1" role="tabpanel">
+                        <div class="tab-pane" id="anamnesis-okupasi" role="tabpanel">
                             <p class="mb-0">
                                 Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum PBR. Homo nostrud organic, assumenda labore aesthetic magna 8-bit.
                             </p>
                         </div>
-                        <div class="tab-pane" id="messages-1" role="tabpanel">
+                        <div class="tab-pane" id="pemeriksaan-fisik" role="tabpanel">
                             <p class="mb-0">
                                 Etsy mixtape wayfarers, ethical wes anderson tofu before they sold out mcsweeney's organic lomo retro fanny pack lo-fi farm-to-table readymade. Messenger bag gentrify pitchfork tattooed craft beer, iphone skateboard locavore carles etsy salvia banksy hoodie helvetica. DIY synth PBR banksy irony. Leggings gentrify squid 8-bit cred pitchfork. Williamsburg banh mi whatever gluten-free.
                             </p>
                         </div>
-                        <div class="tab-pane" id="settings-1" role="tabpanel">
+                        <div class="tab-pane" id="resume" role="tabpanel">
                             <p class="mb-0">
                                 Trust fund seitan letterpress, keytar raw denim keffiyeh etsy art party before they sold out master cleanse gluten-free squid scenester freegan cosby sweater. Fanny pack portland seitan DIY, art party locavore wolf cliche high life echo park Austin. Cred vinyl keffiyeh DIY salvia PBR, banh mi before they sold out farm-to-table.
                             </p>
